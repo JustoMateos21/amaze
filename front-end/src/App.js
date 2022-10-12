999deb318b5e6ea248bcc35c06c20bd9caec4262
JustoMateos21 <98715129+JustoMateos21@users.noreply.github.com>
2022-10-12 20:45:20 -0300

    change -04

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amaze</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
