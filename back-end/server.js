a59271f938b0ba87d5226e1f2a03cd493c24ac91
JustoMateos21 <98715129+JustoMateos21@users.noreply.github.com>
2022-10-25 14:40:17 -0300

    Upload-to-heroku

---
+++ back-end/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRouter.js";
+import productRouter from "./routes/productRouter.js";
+import userRouter from "./routes/userRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+
+const app = express();
+dotenv.config();
+const uri = process.env.MONGODB_URI;
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
+
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/front-end/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/front-end/build/index.html"))
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 4400;
+app.listen(port, () => {
+  console.log(`server at http://localhost:${port}`);
+});
